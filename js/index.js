a7b5f80f5a81d7d436b87d0039159bf0282c8285
W-Alexander <alexanderaguilar726@gmail.com>
2024-05-16 15:46:10 -0600

    Adición de el llamamiento de los contenedores a js

---
+++ js/index.js
@@ -0,0 +1,28 @@
+let cuadro1 = document.getElementById('cuadro-uno');
+
+let cuadro2 = document.getElementById('cuadro-dos');
+
+let cuadro3 = document.getElementById('cuadro-tres');
+
+let cuadro4 = document.getElementById('cuadro-cuatro');
+
+let cuadro5 = document.getElementById('cuadro-cinco');
+
+let cuadro6 = document.getElementById('cuadro-seis');
+
+let cuadro7 = document.getElementById('cuadro-siete');
+
+let cuadro8 = document.getElementById('cuadro-ocho');
+
+let cuadro9 = document.getElementById('cuadro-nueve');
+
+let botonReinicio = document.getElementById('btn');
+
+function selección1(cuadro1) {
+
+
+  let equix = document.createElement('p');
+  equix = "x";
+  
+  
+}
