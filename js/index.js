4a08b21318bd594bcb9efead40f03d2a565ea38f
W-Alexander <alexanderaguilar726@gmail.com>
2024-05-24 13:59:38 -0600

    Prueba05 Final?

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 let listaHTML = document.querySelectorAll('.casillas');
+//llamamos a todos los div´s con el selector "query".
 
 let casillas = [];
+//Creamos un arreglo vacio para introducir los nodos creados de "listaHTML"
+//recorremos a la variable "listaHTML" con un for para permitirnos introducirlos en "casilla" (array).
 for (let index = 0; index < listaHTML.length; index++) {
   casillas.push(listaHTML[index]);
 }
-console.log(casillas);
 //Llama a todos los div's utilizando una misma "clase". 
 
 let botonReinicio = document.getElementById('btn');
@@ -13,18 +15,7 @@ let botonReinicio = document.getElementById('btn');
 let informaciónJuego = document.getElementById('informaciónJuego');
 //LLama el espacio que va a mostrar al usuario lo información del juego, durante el juego.
 
-let condicionesParaGanar = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6],
-];
-console.log(condicionesParaGanar);
-//Creamos una variable que muestre todas las formas de ganar utilizando el indice de las posiciones de los div´s.
+let informaciónJuego2 = document.getElementById('informaciónJuego2');
 
 let jugador = "X"
 //Creamos una variable que tiene de valor un "string".
@@ -45,67 +36,13 @@ function comenzarJuego(){
     casillas[index].addEventListener('click', function () {
       
       //Una vez el click suceda entonces la casilla seleccionada será asignada a un nuevo valor siendo este "X" (valor de jugador)
-      casillas[index].textContent = jugador;
+      casillas[index].innerHTML = jugador;
 
-     validaGanar()
+      //Al mismo tiempo ejecutamos diferentes funciones que validan los espacios para determinar un gane ó empate. 
+      validaGanar()
 
-     function validaGanar() {
-      
-      //for (let index2 = 0; index2 < condicionesParaGanar.length; index2++) {
+      validaEmpates()
 
-        if (casillas[0].textContent === "X" && casillas[1].textContent === "X" && casillas[2].textContent === "X"
-        || casillas[3].textContent === "X" && casillas[4].textContent === "X" && casillas[5].textContent === "X"
-        || casillas[6].textContent === "X" && casillas[7].textContent === "X" && casillas[8].textContent === "X"
-        || casillas[0].textContent === "X" && casillas[3].textContent === "X" && casillas[6].textContent === "X"
-        || casillas[0].textContent === "X" && casillas[3].textContent === "X" && casillas[6].textContent === "X"
-        || casillas[1].textContent === "X" && casillas[4].textContent === "X" && casillas[7].textContent === "X" 
-        || casillas[2].textContent === "X" && casillas[5].textContent === "X" && casillas[8].textContent === "X"
-        || casillas[0].textContent === "X" && casillas[4].textContent === "X" && casillas[8].textContent === "X"
-        || casillas[2].textContent === "X" && casillas[4].textContent === "X" && casillas[6].textContent === "X") {
-
-          return true;
-          
-        } else if (casillas[0].textContent === "O" && casillas[1].textContent === "O" && casillas[2].textContent === "O"
-        || casillas[3].textContent === "O" && casillas[4].textContent === "O" && casillas[5].textContent === "O"
-        || casillas[6].textContent === "O" && casillas[7].textContent === "O" && casillas[8].textContent === "O"
-        || casillas[0].textContent === "O" && casillas[3].textContent === "O" && casillas[6].textContent === "O"
-        || casillas[0].textContent === "O" && casillas[3].textContent === "O" && casillas[6].textContent === "O"
-        || casillas[1].textContent === "O" && casillas[4].textContent === "O" && casillas[7].textContent === "O" 
-        || casillas[2].textContent === "O" && casillas[5].textContent === "O" && casillas[8].textContent === "O"
-        || casillas[0].textContent === "O" && casillas[4].textContent === "O" && casillas[8].textContent === "O"
-        || casillas[2].textContent === "O" && casillas[4].textContent === "O" && casillas[6].textContent === "O") {
-
-          return false;
-          
-        } {
-        }
-      //}
-     }
-
-     if (validaGanar() == true) {
-      informaciónJuego2.innerHTML= "Gana jugador X"// <------estudiar como llamar una etiqueta de manera directa sin declararla.
-
-      informaciónJuego.style.display="none"
-      
-     } else if (validaGanar() == false) {
-
-      informaciónJuego2.innerHTML= "Gana jugador O"// <------estudiar como llamar una etiqueta de manera directa sin declararla.
-
-      informaciónJuego.style.display="none"
-      
-     } {
-     
-      
-     }
-
-     /*if (validaGanar()) {
-    
-      informaciónJuego2.innerHTML= "Gana jugador X"// <------estudiar como llamar una etiqueta de manera directa sin declararla.
-
-      informaciónJuego.style.display="none"
-     }*/
-     
-      
       //Creamos una validación diciendo que mientras jugador tenga un valor estricto de "X"(cadena de texto).
       if ( jugador == "X" ) {
 
@@ -126,32 +63,38 @@ function comenzarJuego(){
         //Recorremos el array vacio que creamos al inicio.
         for (let index = 0; index < vacio.length; index++) {
           
-          //Y decimos
-
+          //Y decimos que sí el array que contiene las posiciones vacias son verdaderas ( sí, (booleano)).
           if (vacio[index] == true ) {
 
+            //entonces al array vacio se le van a agregar todas esas posiciones vacias disponibles.
             espaciosDisponibles.push(index);
           }
           
         }
 
+        //Creamos otra condición diciendo que sí el tamaño del array "espaciosDisponibles" es mayor a "0".
         if (espaciosDisponibles.length>0) {
           
           //Ahora con la función para ejecutar una función dentro de un intervalo de tiempo, decimos que "pc2" va a moverse al hacer click y después de 1 seg.  
           setTimeout(function(){
-    
+            
+            //Siendo entonces declarado "pc2" teniendo la capacidad para moverse de manera aleatoria en las opciones disponibles del array "espaciosDisponibles".
             let pc2 = espaciosDisponibles[Math.floor(Math.random()*espaciosDisponibles.length)];
-    
-           casillas[pc2].textContent = "O";
-      
-           //Al mismo tiempo mostramos en pantalla el turno del jugador.
+
+            //Ahora decimos que el array "casillas" con la selección de "pc2", tendra una inserción de "O".
+            casillas[pc2].innerHTML = "O";
+            
+            //Al mismo tiempo ejecutamos diferentes funciones que validan los espacios para determinar un gane ó un empate.
+            validaGanar()
+           
+            validaEmpates()
+     
+           //Tambien mostramos en pantalla el turno del jugador.
             informaciónJuego.innerHTML = "Turno jugador" + " " + jugador;
      
           }, 1000);
-        }
- 
+        } 
     } 
-
     }); 
   } 
 }
@@ -166,16 +109,53 @@ function reiniciar() {
     //Y asignamos un nuevo valor a las posiciones de los contenedores siendo iguales a "vacio".
     casillas[index].textContent = "";
     
+    //También limpiamos el contenido de "información" para que se quede en blanco cada vez que se toca el botón.
     informaciónJuego2.innerHTML = "";
-
-    informaciónJuego.innerHTML= "Turno jugador" + " " +  jugador;
-    //Así mismo muestra al Usuario información de la situación.
-
   }
 }
 
 
+//Declaramos la función que se encarga de validar los ganes.
+function validaGanar() {
 
+      //De manera manual decimos que si las casillas con la posición especificada contiene el string "X" entonces será un gane.
+    if (casillas[0].textContent === "X" && casillas[1].textContent === "X" && casillas[2].textContent === "X"
+    || casillas[3].textContent === "X" && casillas[4].textContent === "X" && casillas[5].textContent === "X"
+    || casillas[6].textContent === "X" && casillas[7].textContent === "X" && casillas[8].textContent === "X"
+    || casillas[0].textContent === "X" && casillas[3].textContent === "X" && casillas[6].textContent === "X"
+    || casillas[0].textContent === "X" && casillas[3].textContent === "X" && casillas[6].textContent === "X"
+    || casillas[1].textContent === "X" && casillas[4].textContent === "X" && casillas[7].textContent === "X" 
+    || casillas[2].textContent === "X" && casillas[5].textContent === "X" && casillas[8].textContent === "X"
+    || casillas[0].textContent === "X" && casillas[4].textContent === "X" && casillas[8].textContent === "X"
+    || casillas[2].textContent === "X" && casillas[4].textContent === "X" && casillas[6].textContent === "X") {
 
+      
+      //Mostramos el anuncio de la victoria insertandolo en la variable responsable "informaciónJuego2".
+      informaciónJuego2.innerHTML="HAZ GANADO JUGADOR X"
+      alert('Juego Terminado')
+      
+      //retornamos el valor verdadero sí todo esto se cumple.
+      return true
+    } 
+    //También junto con la condición de arriba y al mismo tiempo comparamos los espacios de casillas y si estos contienen al string "O".
+    if (casillas[0].textContent === "O" && casillas[1].textContent === "O" && casillas[2].textContent === "O"
+    || casillas[3].textContent === "O" && casillas[4].textContent === "O" && casillas[5].textContent === "O"
+    || casillas[6].textContent === "O" && casillas[7].textContent === "O" && casillas[8].textContent === "O"
+    || casillas[0].textContent === "O" && casillas[3].textContent === "O" && casillas[6].textContent === "O"
+    || casillas[0].textContent === "O" && casillas[3].textContent === "O" && casillas[6].textContent === "O"
+    || casillas[1].textContent === "O" && casillas[4].textContent === "O" && casillas[7].textContent === "O" 
+    || casillas[2].textContent === "O" && casillas[5].textContent === "O" && casillas[8].textContent === "O"
+    || casillas[0].textContent === "O" && casillas[4].textContent === "O" && casillas[8].textContent === "O"
+    || casillas[2].textContent === "O" && casillas[4].textContent === "O" && casillas[6].textContent === "O") {
+   
+      
+      //Mostramos el anuncio de la victoria insertandolo en la variable responsable "informaciónJuego2".
+      informaciónJuego2.innerHTML="HAZ GANADO JUGADOR O"
 
+      //retornamos el valor verdadero sí todo esto se cumple.
+      return true
+    }
+  
+} 
 
+// Te debo el empate...
